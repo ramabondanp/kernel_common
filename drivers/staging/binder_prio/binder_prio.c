bb188e2f33426a011a08393f9a5f7db1c870c922
Rama Bondan Prakoso <ramanarubp@gmail.com>
2024-12-25 20:10:19 +0700

    Revert "drivers: staging: binder_prio: Increase the priority for hilauncher"
    This reverts commit fd53942cee2cbfd5480b2c0459601e1c7cd70a71.

--- drivers/staging/binder_prio/binder_prio.c
+++ drivers/staging/binder_prio/binder_prio.c
@@ -12,7 +12,6 @@ static const char *task_name[] = {
 	"com.miui.home",
 	"droid.launcher3",  // com.android.launcher3
 	"ion.XOSLauncher", // com.transsion.XOSLauncher
-	"sion.hilauncher", // com.transsion.hilauncher
 	"ndroid.systemui",  // com.android.systemui
 	// "surfaceflinger",
 	"com.tencent.mm",
